b3c59127973c637eb11360b1c3efbbb05b22bacb
rdalkire <dave.alkire@gmail.com>
2019-04-22 21:42:07 -0400

    Add project files.

---
+++ DoFactory-Sample/Controllers/OrderItemsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoFactory_Sample.Models;
+
+namespace DoFactory_Sample.Controllers
+{
+    public class OrderItemsController : Controller
+    {
+        private dofactory_sampleEntities db = new dofactory_sampleEntities();
+
+        // GET: OrderItems
+        public ActionResult Index()
+        {
+            var orderItems = db.OrderItems.Include(o => o.Order).Include(o => o.Product);
+            return View(orderItems.ToList());
+        }
+
+        // GET: OrderItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderItem orderItem = db.OrderItems.Find(id);
+            if (orderItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "OrderNumber");
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "ProductName");
+            return View();
+        }
+
+        // POST: OrderItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,OrderId,ProductId,UnitPrice,Quantity")] OrderItem orderItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OrderItems.Add(orderItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "OrderNumber", orderItem.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "ProductName", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderItem orderItem = db.OrderItems.Find(id);
+            if (orderItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "OrderNumber", orderItem.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "ProductName", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // POST: OrderItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,OrderId,ProductId,UnitPrice,Quantity")] OrderItem orderItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "OrderNumber", orderItem.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "ProductName", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderItem orderItem = db.OrderItems.Find(id);
+            if (orderItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderItem);
+        }
+
+        // POST: OrderItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OrderItem orderItem = db.OrderItems.Find(id);
+            db.OrderItems.Remove(orderItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# DoFactory-Sample
+
+Starting from [SQL Tutorial Sample Database](https://www.dofactory.com/sql/sample-database) at dofactory.com
+
+My plan (as of this writing) is to create a kind of grid-dropdown using [react-autocomplete](https://github.com/reactjs/react-autocomplete).
