67db4007d5a48c16bc21daaf299a9a0b5b36eeaa
Dave Alkire <dave.alkire@gmail.com>
2019-04-26 16:50:15 -0400

    Began to port the app so far to dotnet core.  Scaffolded the model.

---
+++ DoFactory_Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DoFactory_Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
